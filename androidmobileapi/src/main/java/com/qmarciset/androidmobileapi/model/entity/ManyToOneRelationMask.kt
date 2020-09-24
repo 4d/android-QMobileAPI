aaac8f68c23adadcf67fa8adcc6193e32ca52ba8
Quentin Marciset <quentin.marciset@4d.com>
2020-09-24 16:02:39 +0200

    Improved relation handling for deeper levels added JSON utility functions

---
+++ androidmobileapi/src/main/java/com/qmarciset/androidmobileapi/model/entity/ManyToOneRelationMask.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by Quentin Marciset on 23/9/2020.
+ * 4D SAS
+ * Copyright (c) 2020 Quentin Marciset. All rights reserved.
+ */
+
+package com.qmarciset.androidmobileapi.model.entity
+
+@Suppress("ConstructorParameterNaming")
+data class ManyToOneRelationMask(
+    val __deferred: DeferredManyToOneRelationMask?
+)
