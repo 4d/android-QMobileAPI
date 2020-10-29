f70e3e628221b6d358554dace52ed8eb506884d9
Quentin Marciset <quentin.marciset@4d.com>
2020-10-29 10:56:55 +0100

    removed obselete code

--- qmobileapi/src/main/java/com/qmobile/qmobileapi/utils/Constants.kt
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/utils/Constants.kt
@@ -6,9 +6,6 @@
 
 package com.qmobile.qmobileapi.utils
 
-// Obsolete
-const val BASE_URL = "http://192.168.5.10:8099/mobileapp/"
-
 // Set to `true` if cookies WASID4D and WAKTRANSID should be used with headers
 const val COOKIES_ARE_HANDLED = false
 
