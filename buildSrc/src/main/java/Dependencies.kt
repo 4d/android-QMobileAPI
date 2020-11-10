488b8f33b57fa58f8ea49cc281974005f6bfeb19
Quentin Marciset <quentin.marciset@4d.com>
2020-11-10 09:25:49 +0100

    update gradle wrapper version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
     val lifecycle = "2.2.0"
     val mockito = "3.6.0"
     val multidex = "2.0.1"
-    val navigation = "2.3.1"
+    val navigation = "2.3.0"
     val okhttp = "4.9.0"
     val preference = "1.1.1"
     val retrofit = "2.9.0"
