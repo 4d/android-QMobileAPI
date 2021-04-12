3aec65c564529a4b9b7f4ee89a06634cbb74fc1d
Quentin Marciset <quentin.marciset@4d.com>
2021-04-12 16:05:07 +0200

    Implement server accessibility request #126514

---
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/network/AccessibilityApiService.kt
@@ -0,0 +1,22 @@
+/*
+ * Created by Quentin Marciset on 12/4/2021.
+ * 4D SAS
+ * Copyright (c) 2021 Quentin Marciset. All rights reserved.
+ */
+
+package com.qmobile.qmobileapi.network
+
+import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AccessibilityApiService {
+
+    /*
+     * SERVER ACCESSIBILITY
+     */
+
+    @GET("/")
+    fun checkAccessibility(): Single<Response<ResponseBody>>
+}
