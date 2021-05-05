19bf535f56d6b6770773172a7b4cd61cd749c243
Quentin Marciset <quentin.marciset@4d.com>
2021-05-05 15:57:26 +0200

    add utf8 const

--- qmobileapi/src/main/java/com/qmobile/qmobileapi/utils/Constants.kt
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/utils/Constants.kt
@@ -21,6 +21,7 @@ const val DELETED_RECORDS = "__DeletedRecords"
 // POST request body information
 const val APP_JSON = "application/json"
 const val UTF8_CHARSET = "charset=utf-8"
+const val UTF8 = "utf-8"
 
 // 4D server globalStamp property name
 const val GLOBALSTAMP_PROPERTY = "__GlobalStamp"
