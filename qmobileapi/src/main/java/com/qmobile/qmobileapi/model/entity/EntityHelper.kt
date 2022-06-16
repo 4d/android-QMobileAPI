9ccbe380c20efbe552ad4150e60ee651aa548261
Quentin Marciset <quentin.marciset@4d.com>
2022-06-16 17:56:33 +0200

    remove useless lowercase transformation

--- qmobileapi/src/main/java/com/qmobile/qmobileapi/model/entity/EntityHelper.kt
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/model/entity/EntityHelper.kt
@@ -6,18 +6,14 @@
 
 package com.qmobile.qmobileapi.model.entity
 
-import java.util.Locale
 import kotlin.reflect.KProperty1
 
 class EntityHelper private constructor() {
     companion object {
         @Suppress("UNCHECKED_CAST")
-        fun <R> readInstanceProperty(instance: Any, propertyName: String): R {
-            val property = instance::class.members
-                .first {
-                    it.name.lowercase(Locale.getDefault()) == propertyName.lowercase(Locale.getDefault())
-                } as KProperty1<Any, *>
-            return property.get(instance) as R
+        fun <R> readInstanceProperty(instance: Any, propertyName: String): R? {
+            val property = instance::class.members.first { it.name == propertyName } as KProperty1<Any, *>?
+            return property?.get(instance) as R?
         }
     }
 }
