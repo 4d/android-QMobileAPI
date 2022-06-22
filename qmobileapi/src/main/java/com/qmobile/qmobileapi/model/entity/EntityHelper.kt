7a207df4b6602f600b3dc075558e33ae1ec6130c
Quentin Marciset <quentin.marciset@4d.com>
2022-06-22 17:53:26 +0200

    rollback modification on kotlin reflection

--- qmobileapi/src/main/java/com/qmobile/qmobileapi/model/entity/EntityHelper.kt
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/model/entity/EntityHelper.kt
@@ -11,9 +11,9 @@ import kotlin.reflect.KProperty1
 class EntityHelper private constructor() {
     companion object {
         @Suppress("UNCHECKED_CAST")
-        fun <R> readInstanceProperty(instance: Any, propertyName: String): R? {
-            val property = instance::class.members.first { it.name == propertyName } as KProperty1<Any, *>?
-            return property?.get(instance) as R?
+        fun <R> readInstanceProperty(instance: Any, propertyName: String): R {
+            val property = instance::class.members.first { it.name == propertyName } as KProperty1<Any, *>
+            return property.get(instance) as R
         }
     }
 }
