2a45b093fb0479a0626cfcf454c780711591ada5
Quentin Marciset <quentin.marciset@4d.com>
2022-10-27 15:28:52 +0200

    fix issue with not valid port leading to crash

--- qmobileapi/src/main/java/com/qmobile/qmobileapi/auth/InputTextValidation.kt
+++ qmobileapi/src/main/java/com/qmobile/qmobileapi/auth/InputTextValidation.kt
@@ -17,3 +17,9 @@ fun String.isEmailValid(): Boolean = Patterns.EMAIL_ADDRESS.toRegex().matches(th
  * Verifies that the current String is a server address
  */
 fun String.isUrlValid(): Boolean = Patterns.WEB_URL.toRegex().matches(this)
+
+/**
+ * Verifies that the current String is a valid port
+ */
+fun String.isPortValid(): Boolean =
+    "^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])\$".toRegex().matches(this)
